e28ffa828305086704f3f3066099749587ad4173
muh rizvan <clgrizvan@gmail.com>
2025-10-02 15:59:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { UserProvider } from './Store/UserContext'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <UserProvider>
+  <React.StrictMode>
     <App />
-  </UserProvider>
+  </React.StrictMode>
 );
-
